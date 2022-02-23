b75b677e18eebbd2d0ac29ac4fca2d99c058f600
howsilence <statton0104@gmail.com>
2022-02-22 20:42:39 -0500

    commit tree 0ec043a1fde9a639fd59302729c0c12b538b9f1f

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,30 @@
 class Dog
 
+    # attr_accessor :name
+    # attr_accessor :breed
+
+    # def initialize(name, breed)
+
+    #     @name = name
+
+    #     @breed = breed
+    
+    # end
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
+
 end
